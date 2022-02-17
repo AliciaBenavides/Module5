29d02114b208abe70d4a2a41937470af1ade8e08
AliciaBenavides <77942770+AliciaBenavides@users.noreply.github.com>
2022-02-16 21:19:00 -0500

    update

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const names = ["Professor", "Classmates"];
+const n = 5;// for loop example
 
-for (name of names) {
-    console.log('Welcome ${name}');
+for (let i = 1; i <= n; i++) {
+    console.log('This is my for Loop example.');
 }
+
+let count = 1;        // while loop example
+while (count < 10) {
+    console.log(count);
+    count +=2;
+}
+
+
+
+const div = document.getElementById("container"); //change CSS with JS
+div.style.color = "darkblue";
+
+
+
+let price = "SOLD OUT";  // change CSS based upon a variable value
+document.getElementById("demo").innerHTML = price;
+demo.style.color = "red";
